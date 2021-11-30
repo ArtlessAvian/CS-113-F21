5f8b7706d97143dc3e8e3fb93f8476cbb2d69215
Ryan <artlessavian@gmail.com>
2021-11-29 23:35:43 -0800

    tree sorter

---
+++ Assets/Scripts/TreeSort.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TreeSort : MonoBehaviour
+{
+    public bool sortTrees;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sortTrees)
+        {
+            sortTrees = false;
+
+            foreach (SpriteRenderer renderer in FindObjectsOfType<SpriteRenderer>())
+            {
+                if (renderer?.sprite?.name == "tree1")
+                {
+                    Vector3 copy = renderer.transform.position;
+                    copy.z = -1 + copy.y/100;
+
+                    renderer.transform.position = copy;
+                }
+            }
+        }
+    }
+}
