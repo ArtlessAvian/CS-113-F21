fa7251dedcf7cc3f7f0756231cdf98adefb17ac7
UCImjmata <52990221+UCImjmata@users.noreply.github.com>
2021-12-04 15:47:16 -0800

    Added Bullet Pickup Sfx

--- Assets/Scripts/BulletPickup.cs
+++ Assets/Scripts/BulletPickup.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BulletPickup : MonoBehaviour
 {
+    public AudioSource reload_sfx;
+    public float volume = 1.0f;
+    public Renderer bullet_graphic;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
@@ -11,12 +14,15 @@ public class BulletPickup : MonoBehaviour
             if (collision.GetComponent<PlayerController>().ammoCount < 6)
             {
                 collision.GetComponent<PlayerController>().ammoCount += 1;
+                Debug.Log("Playing pickup audio");
+                reload_sfx.PlayOneShot(reload_sfx.clip, volume);
+                //reload_sfx.PlayDelayed(0f);
+                bullet_graphic.enabled = false;
+                Destroy(this.gameObject, reload_sfx.clip.length);
             }
-            this.enabled = false;
-            Destroy(this.gameObject, 0.1f);
 
             // play reload sound.
-            
+
 
         }
     }
